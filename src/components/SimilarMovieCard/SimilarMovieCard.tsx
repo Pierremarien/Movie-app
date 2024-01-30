574fc87969a41b8b5bdcb81f6007a75f35952723
pierre <pierremarien29@gmail.com>
2024-01-30 20:40:41 +0100

    similar movie card done

--- src/components/SimilarMovieCard/SimilarMovieCard.tsx
+++ src/components/SimilarMovieCard/SimilarMovieCard.tsx
@@ -16,6 +16,12 @@ export const SimilarMovieCard = () => {
       </figure>
       <div className={styles["card__info"]}>
         <h2 className={styles["card__info__title"]}>Aquaman et les poissons</h2>
+        <div className={styles["card__info__genre"]}>
+          {/* display only the first four genres for a movie here the rest will be in the detailed movie page if there are more than 4*/}
+          <button className={styles["card__info__genre__pill"]}>genre</button>
+          <button className={styles["card__info__genre__pill"]}>genre</button>
+          <button className={styles["card__info__genre__pill"]}>genre</button>
+        </div>
         <div className={styles["card__info__stats"]}>
           <p className={styles["card__info__stats__vote"]}>vote</p>
           <p className={styles["card__info__stats__date"]}>date date date</p>
